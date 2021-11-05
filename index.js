b53f1033c190474b05ca9d3f9083fcc9bdc60f3b
Will <twitchifygamers@gmail.com>
2021-11-05 11:02:56 +0000

    Initial commit with index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents:
+        [
+            Intents.FLAGS.GUILDS,
+            Intents.FLAGS.GUILD_MESSAGES
+        ]
+});
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(token);
